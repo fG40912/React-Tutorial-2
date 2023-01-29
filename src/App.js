8ed691ada2813c29ca86a675a91f4bc7eaffe850
FutureNoogler <futureg412@gmail.com>
2023-01-29 10:33:43 +0000

    lesson 01 - useState()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
 
 function App() {
+  const [ count, setCount ] = useState(4)
+  const [ theme, setTheme ] = useState('blue')
+
+  function decrementCount(){
+    setCount(prevCount => { return count-1 })
+    setTheme(() => 'blue')
+  }
+
+  function incrementCount(){
+    setCount(prevCount => { return count+1 })
+    setTheme(() => 'red')
+  }
+
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
