911ba59d22ce795a40fed0e9064c7f31bdb6b4a9
fG40912 <fG40912@gmail.com>
2023-01-30 03:10:49 +0000

    step 2. fetch data from URL + double rendering on window.reload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-    const [resourceType, setResourceType] = useState('Posts')
+    const [resourceType, setResourceType] = useState('posts')
+
+    useEffect(() => {
+            fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then(reponse => reponse.json())
+            .then(json => console.log(json))
+        }, [resourceType])
 
     return(
         <>
-            <button onClick={() => setResourceType('Posts') }>Posts</button>
-            <button onClick={() => setResourceType('Users') }>Users</button> 
-            <button onClick={() => setResourceType('Comments') }>Comments</button> 
+            <button onClick={() => setResourceType('posts') }>Posts</button>
+            <button onClick={() => setResourceType('users') }>Users</button> 
+            <button onClick={() => setResourceType('comments') }>Comments</button> 
             <h1>{ resourceType }</h1>
         </>
     )
